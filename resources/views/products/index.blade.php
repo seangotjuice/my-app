560cef3e6bf548391b46873a17029e14ccd8dfa8
Sean Chen <seanchen@Atl-Tech-SeanChen.local>
2024-04-22 09:58:51 +0800

    feat: Show all products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,7 +7,29 @@
 </head>
 <body>
     <h1>Product</h1>
-    <div>index</div>
+    <div>
+        <table border = "1">
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Qty</th>
+                <th>Description</th>
+                <th>Edit</th>
+            </tr>
+            @foreach($products as $product)
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->name}}</td>
+                    <td>{{$product->price}}</td>
+                    <td>{{$product->qty}}</td>
+                    <td>{{$product->description}}</td>
+                    <td></td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+   
     
 </body>
 </html>
